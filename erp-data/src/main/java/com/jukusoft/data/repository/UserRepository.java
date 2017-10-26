3f62a9b2172f447b9fb2caf87d6c27710d4604df
JuKu <kuenzel.justin@t-online.de>
2017-10-27 01:57:20 +0200

    added user repository.

---
+++ erp-data/src/main/java/com/jukusoft/data/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.jukusoft.data.repository;
+
+import com.jukusoft.erp.lib.database.AbstractMySQLRepository;
+
+public class UserRepository extends AbstractMySQLRepository {
+
+    /*public static boolean checkPassword (long userID, String password) {
+        //TODO: read user from table
+
+        //hash password
+        String hash = HashUtils.computeSHA256Hash(password)
+    }*/
+
+    //
+
+}
