abd451dda093a8977df15c8bbb80cb35ff017c35
JuKu <kuenzel.justin@t-online.de>
2017-11-07 09:56:43 +0100

    changed interface ICache, added support to cache json arrays too.

--- erp-library/src/main/java/com.jukusoft/erp/lib/cache/ICache.java
+++ erp-library/src/main/java/com.jukusoft/erp/lib/cache/ICache.java
@@ -1,16 +1,38 @@
 package com.jukusoft.erp.lib.cache;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public interface ICache {
 
     public void put (String key, JsonObject data);
 
+    default void putArray (String key, JsonArray array) {
+        JsonObject obj = new JsonObject();
+        obj.put("array", array);
+
+        put(key, obj);
+    }
+
     public void remove (String key);
 
     public boolean contains (String key);
 
     public JsonObject get (String key);
+
+    default JsonArray getArray (String key) {
+        JsonObject obj = get(key);
+
+        if (obj == null) {
+            return null;
+        }
+
+        if (!obj.containsKey("array")) {
+            throw new IllegalStateException("cached object isnt an array.");
+        }
+
+        return obj.getJsonArray("array");
+    }
 
     public void cleanUp ();
 
